72be6ea2c8ad9910517ccd3056a00be4fc105523
a16711 <a16711@d1-07-02.dped.local>
2016-02-03 13:04:29 +0000

    Alterei algum metodos do Comverter e do ValidaStrings e pos comentarios na classe do Comverter

--- src/figura/Retangulo.java
+++ src/figura/Retangulo.java
@@ -3,7 +3,7 @@ package figura;
 import primitivas.Ponto2D;
 
 /**
- * 
+ * Esta classe permite defenir o canto, a largura e a altura
  * @author BM e NL
  * @version 1
  */
@@ -34,40 +34,75 @@ public class Retangulo {
         this.altura = altura;
         this.canto = canto.clone();
     }
-    
+    /**
+     * Altera o valor da largura do retangulo
+     * @param largura toma o valor da largura
+     */
     public void defineLargura(int largura) {
         this.largura = largura;
     }
+    /***
+     * Altera o valor da altura do retangulo
+     * @param altura toma o valor da altura
+     */
     public void defineAltura(int altura) {
         this.altura = altura;
     }
-    
+    /**
+     * Altera o valor do cantoX e do cantoY do retangulo
+     * @param canto toma o valor de cantoX e cantoY
+     */
     public void defineCanto(Ponto2D canto ) {
         this.canto = new Ponto2D(canto.obterX(), canto.obterY());
     }
+    /**
+     * Devolve o valor da largura
+     * @return valor inteiro da largura
+     */
     public int obterLargura() {
         return largura;
     }
+    /**
+     * Devolve o valor da altura
+     * @return valor inteiro da altura
+     */
     public int obterAltura() {
         return altura;
     }
-    
+    /**
+     * Devolve o valor do canto
+     * @return valor Ponto2D do canto
+     */
     public Ponto2D obterCanto() {
         return canto;
     }
-    
+    /**
+     * Faz a largura*altura
+     * @return devolve o valor da area 
+     */
     public double obterArea() {
         return largura*altura;
     }
-    
+    /**
+     * Faz 2*largura + 2*altura
+     * @return devolve o perimetro do retangulo
+     */
     public double obterPerimetro() {
         return 2*largura + 2*altura;
     }
+    /**
+     * Da o valor ao cantoX e cantoY
+     * @return devolve o valor do cantoX e cantoY
+     */
     @Override
     public String toString() {
         return "Retangulo: Canto "+canto+" Dimensões (" + largura + "x" + altura + ")";
     }
-    
+    /**
+     * Verifica se os cantos estam no retangulo
+     * @param p é o que verifica se iziste os cantosX e cantosY
+     * @return mostra se isiste o pomto se nao é false
+     */
     public boolean estaDentro(Ponto2D p) {
         boolean res = false;
         if (canto.obterX() > p.obterX() && canto.obterX() + largura < p.obterX()) {
