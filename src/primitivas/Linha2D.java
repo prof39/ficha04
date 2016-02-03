234ecbd2b090974122d7b9d3541c8907b5939cf8
a16711 <a16711@d1-07-02.dped.local>
2016-02-03 13:04:29 +0000

    Alterei algum metodos do Comverter e do ValidaStrings e pos comentarios na classe do Comverter

--- src/primitivas/Linha2D.java
+++ src/primitivas/Linha2D.java
@@ -2,7 +2,8 @@ package primitivas;
 
 /**
  * Classe para trabalhar com linhas em duas dimensões
- * @author gm
+ * @author NL e BM
+ * @version 1
  */
 public class Linha2D {
     private Ponto2D p1;
@@ -10,7 +11,12 @@ public class Linha2D {
     private double declive;
     private boolean vertical;
     
-   
+   /**
+    * 
+    * @param p1 toma o valor de p1
+    * @param p2 toma o valor de p2
+    * @throws IllegalArgumentException faz uma exeção
+    */
     public Linha2D(Ponto2D p1, Ponto2D p2) throws IllegalArgumentException {
         
         if(p1.equals(p2)) {
@@ -25,7 +31,14 @@ public class Linha2D {
             calcularDeclive();
         }
     }
-    
+    /**
+     * 
+     * @param x1 toma o valor de p1.x
+     * @param y1 toma o valor de p1.y
+     * @param x2 toma o valor de p2.x
+     * @param y2 toma o valor de p2.y
+     * @throws IllegalArgumentException faz uma exeção
+     */
     public Linha2D(int x1, int y1, int x2, int y2) throws IllegalArgumentException {
        if(x1 == x2 && y1 == y2) {
             throw new IllegalArgumentException("Não é possível criar uma linha com dois pontos iguais");
@@ -51,7 +64,9 @@ public class Linha2D {
         }
         
     }
-    
+    /**
+     * Verifica se é verdadeiro ou false
+     */
     private void verificaDeclive() {
        if(p1.x == p2.x){
             vertical = true;
@@ -60,37 +75,61 @@ public class Linha2D {
             vertical = false;
        }
     }
-   
+   /**
+    * Verifica se as cordenadas dos potes estão bem
+    */
     private void calcularDeclive() {
        declive = (p1.y - p2.y)/(p1.x - p2.x);
     }
-   
+   /**
+    * Devolve o declive
+    * @return devolve o valor de declive
+    */
     public double obterDeclive() {
         return declive;
     }
-    
+    /**
+     * Verifica se a linha é vertical
+     * @return mostra a linha vertical
+     */
     public boolean eVertical() {
         return vertical;
     }
-    
+    /**
+     * Verifica se o res e false 
+     * @param p mostra se pretence ao Ponto2D
+     * @return mostra o resultado de res
+     */
     public boolean pertence(Ponto2D p) {
         boolean res = false;
         
         return res;
     }
-   
+   /**
+    * Verifica se o res é false
+    * @param l mostra se é paralela a Linha2D
+    * @return mostra o resultado do res
+    */
     public boolean paralela(Linha2D l) {
         boolean res = false;
         
         return res;
     }
-    
+    /**
+     * Verifica se o res é false
+     * @param l mostra se é prependicular a Linha2D
+     * @return mostra o valor de res
+     */
     public boolean perpendicular(Linha2D l) {
         boolean res = false;
       
         return res;
     }
-   
+   /**
+    * Verifica se o equal é a Linha2D
+    * @param l mostra se equal faz parte da Linha2D
+    * @return mostra o resultado false
+    */
     public boolean equal(Linha2D l) {
         return false;
     }
